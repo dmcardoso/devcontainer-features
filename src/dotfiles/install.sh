8598e8264257711383cd7c6669528bbd0713ac89
Daniel Moreira <dmcardoso.99@hotmail.com>
2025-05-28 20:12:45 -0300

    try to fix

--- src/dotfiles/install.sh
+++ src/dotfiles/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+set -x
+
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Creates root directories if they don't exist
@@ -8,8 +11,8 @@ mkdir -p "$HOME/.config"
 # Remove existing files if they exist
 rm -f "$HOME/.config/starship.toml"
 rm -f "$HOME/.vimrc"
-rm -f "/etc/fish/config.fish"
+sudo rm -f "/etc/fish/config.fish"
 
 cp "$SCRIPT_DIR/config/starship/starship.toml" "$HOME/.config/starship.toml"
 cp "$SCRIPT_DIR/config/vim/.vimrc" "$HOME/.vimrc"
-cp "$SCRIPT_DIR/config/fish/config.fish" "/etc/fish/config.fish"
+sudo cp "$SCRIPT_DIR/config/fish/config.fish" "/etc/fish/config.fish"
