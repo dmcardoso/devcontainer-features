eb8e788fa05e1843e78b3179a426f6aa928c3ad4
Daniel Moreira <dmcardoso.99@hotmail.com>
2025-05-28 20:47:31 -0300

    add home user option

--- src/dotfiles/install.sh
+++ src/dotfiles/install.sh
@@ -6,13 +6,13 @@ set -x
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Creates root directories if they don't exist
-mkdir -p "$HOME/.config"
+mkdir -p "/home/$HOMEUSER/.config"
 
 # Remove existing files if they exist
-rm -f "$HOME/.config/starship.toml"
-rm -f "$HOME/.vimrc"
+rm -f "/home/$HOMEUSER/.config/starship.toml"
+rm -f "/home/$HOMEUSER/.vimrc"
 sudo rm -f "/etc/fish/config.fish"
 
-cp "$SCRIPT_DIR/config/starship/starship.toml" "$HOME/.config/starship.toml"
-cp "$SCRIPT_DIR/config/vim/.vimrc" "$HOME/.vimrc"
+cp "$SCRIPT_DIR/config/starship/starship.toml" "/home/$HOMEUSER/.config/starship.toml"
+cp "$SCRIPT_DIR/config/vim/.vimrc" "/home/$HOMEUSER/.vimrc"
 sudo cp "$SCRIPT_DIR/config/fish/config.fish" "/etc/fish/config.fish"
