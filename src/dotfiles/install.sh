7afcd863e9e431a03e1db3c055f04e860e5220c0
Daniel Moreira <dmcardoso.99@hotmail.com>
2025-06-03 20:21:59 -0300

    install bat, fzf and eza

--- src/dotfiles/install.sh
+++ src/dotfiles/install.sh
@@ -16,3 +16,12 @@ sudo rm -f "/etc/fish/config.fish"
 cp "$SCRIPT_DIR/config/starship/starship.toml" "/home/$HOMEUSER/.config/starship.toml"
 cp "$SCRIPT_DIR/config/vim/.vimrc" "/home/$HOMEUSER/.vimrc"
 sudo cp "$SCRIPT_DIR/config/fish/config.fish" "/etc/fish/config.fish"
+
+mkdir -p /etc/apt/keyrings
+
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+
+sudo apt update
+sudo apt install -y gpg eza bat fzf vim
